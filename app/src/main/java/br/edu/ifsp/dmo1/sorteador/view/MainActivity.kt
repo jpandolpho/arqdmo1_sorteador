a186e9acf7b62701e428be696e93b53f05e254b9
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-11-08 15:00:47 -0300

    Implementação da funcionalidade.

--- app/src/main/java/br/edu/ifsp/dmo1/sorteador/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/sorteador/view/MainActivity.kt
@@ -1,21 +1,66 @@
 package br.edu.ifsp.dmo1.sorteador.view
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import br.edu.ifsp.dmo1.sorteador.R
+import br.edu.ifsp.dmo1.sorteador.databinding.ActivityMainBinding
+import br.edu.ifsp.dmo1.sorteador.model.Draw
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener{
+    private lateinit var binding: ActivityMainBinding
+    private var draw = Draw()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setClickListener()
+    }
+
+    override fun onClick(v: View?) {
+        when(v){
+            binding.buttonUseLimit ->{
+                val limit:Int = try{
+                    binding.editLimit.text.toString().toInt()
+                }catch(e: NumberFormatException){
+                    -1
+                }
+                draw = if(limit>1)
+                    Draw(limit)
+                else
+                    Draw()
+                updateUI()
+            }
+            binding.buttonDraw -> {
+                binding.textviewExit.text = draw.getNumber().toString()
+                updateListview()
+            }
         }
+    }
+
+    private fun updateListview() {
+        val adapter: ArrayAdapter<Int> = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            draw.getHistory()
+        )
+        binding.listviewDraw.adapter = adapter
+    }
+
+    private fun updateUI() {
+        val str = String.format("Intervalo de 1 à %,d.", draw.getHighBorder())
+        binding.textviewInterval.text = str
+        binding.editLimit.text.clear()
+        binding.textviewExit.text = getString(R.string.inicie_o_sorteio)
+        updateListview()
+    }
+
+    private fun setClickListener() {
+        binding.buttonDraw.setOnClickListener(this)
+        binding.buttonUseLimit.setOnClickListener(this)
     }
 }
